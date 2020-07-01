6b73909c59b0dc60da615fc4e628ac389a69d5d8
lukas1us <lukas1us@users.noreply.github.com>
2020-07-01 20:06:44 +0200

    Updates

---
+++ precache-manifest.2bd4eb0fa6bea5e61a9dff36b4b95e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8dc56708da1ff27acd4303d85bea4bc",
+    "url": "/covid19_app/index.html"
+  },
+  {
+    "revision": "c5730ffec6842240fd68",
+    "url": "/covid19_app/static/css/main.2b293512.chunk.css"
+  },
+  {
+    "revision": "360ec358d27428a4aed1",
+    "url": "/covid19_app/static/js/2.28b09d63.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_app/static/js/2.28b09d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5730ffec6842240fd68",
+    "url": "/covid19_app/static/js/main.2dae517c.chunk.js"
+  },
+  {
+    "revision": "93001360b2ad13bd79ec",
+    "url": "/covid19_app/static/js/runtime-main.144712f5.js"
+  }
+]);
